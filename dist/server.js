1b4566dd8e17536111097a1c816d14199707d7b7
Saulo2706 <saulojr02@outlook.com>
2022-07-13 10:05:35 -0300

    Instalando e configurando CORS

--- dist/server.js
+++ dist/server.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const funcionarios_controller_1 = require("./controllers/funcionarios.controller");
 const funcionarios_routes_1 = require("./routes/funcionarios.routes");
 const node_cron_1 = __importDefault(require("node-cron"));
@@ -12,5 +13,13 @@ node_cron_1.default.schedule("*/10 * * * *", () => {
     (0, funcionarios_controller_1.requestDatasulPessoasPresentes)();
 });
 const app = (0, express_1.default)();
+app.use((req, res, next) => {
+    //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+    //Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use((0, cors_1.default)());
+    next();
+});
 app.use(funcionarios_routes_1.funcionariosRoutes);
 app.listen(3333, () => console.log("Server On"));
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { requestDatasulPessoasPresentes } from "./controllers/funcionarios.controller";
 import { funcionariosRoutes } from "./routes/funcionarios.routes";
 import cron from "node-cron";
@@ -9,6 +10,15 @@ cron.schedule("*/10 * * * *", () => {
 });
 
 const app = express();
+
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 app.use(funcionariosRoutes);
 
